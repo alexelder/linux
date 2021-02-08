2dfdc5cef2c89ba6ef73c9010fdd269e35d7b34f
Alex Elder <elder@linaro.org>
2021-02-08 14:25:00 -0600

    net: ipa: add IPA v4.7 configuration data
    Signed-off-by: Alex Elder <elder@linaro.org>

--- drivers/net/ipa/ipa_data.h
+++ drivers/net/ipa/ipa_data.h
@@ -306,6 +306,7 @@ extern const struct ipa_data ipa_data_v4_0;
 extern const struct ipa_data ipa_data_v4_1;
 extern const struct ipa_data ipa_data_v4_2;
 extern const struct ipa_data ipa_data_v4_5;
+extern const struct ipa_data ipa_data_v4_7;
 extern const struct ipa_data ipa_data_v4_9;
 extern const struct ipa_data ipa_data_v4_11;
 
