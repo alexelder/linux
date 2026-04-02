1e7729cba39f44bf690ab57e2b44960bc394dfe0
Alex Elder <elder@riscstar.com>
2026-04-01 21:41:34 -0500

    block: csci5980_blk: initialize a tag set
    Create a trivial queue_rq callback function, and set it as the
    callback function in a new global blk_mq_ops structure.
    
    Use that structure to initialize another new global, a tag set.
    
    Signed-off-by: Alex Elder <elder@umn.edu>

--- drivers/block/csci5980_blk.c
+++ drivers/block/csci5980_blk.c
@@ -1,12 +1,26 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <linux/blkdev.h>
+#include <linux/blk-mq.h>
 #include <linux/module.h>
 
 #define DRIVER_NAME	"csci5980_blk"
 
 /* Allocated major device number */
 static unsigned int major;
+
+/* Tag set used for requests */
+static struct blk_mq_tag_set tag_set;
+
+static blk_status_t csci5980_queue_rq(struct blk_mq_hw_ctx *hctx,
+				      const struct blk_mq_queue_data *bd)
+{
+	return BLK_STS_OK;
+}
+
+struct blk_mq_ops csci5980_mq_ops = {
+	.queue_rq	= csci5980_queue_rq,
+};
 
 static int __init csci5980_init(void)
 {
@@ -19,11 +33,25 @@ static int __init csci5980_init(void)
 	major = ret;
 	printk(" === major device number %u\n", major);
 
+	tag_set.ops = &csci5980_mq_ops;
+	tag_set.nr_hw_queues = 1;
+	tag_set.queue_depth = BLKDEV_DEFAULT_RQ;
+	tag_set.numa_node = NUMA_NO_NODE;
+	ret = blk_mq_alloc_tag_set(&tag_set);
+	if (ret)
+		goto err_unregister_blkdev;
+
 	return 0;
+
+err_unregister_blkdev:
+	unregister_blkdev(major, DRIVER_NAME);
+
+	return ret;
 }
 
 static void __exit csci5980_exit(void)
 {
+	blk_mq_free_tag_set(&tag_set);
 	unregister_blkdev(major, DRIVER_NAME);
 }
 
