031312f30023fea4bb2fb50c7fb7325813e59aea
Alex Elder <elder@riscstar.com>
2026-06-26 07:40:18 -0500

    tc956x: have all clocks be initially disabled
    Disable all clocks initially.  This requires any clock user to explicitly
    state that it requires a clock and enable it.
    
    Also determine the two offsets used for clocks based on the values
    in devicetree.
    
    Signed-off-by: Alex Elder <elder@riscstar.com>

--- drivers/clk/clk-tc956x.c
+++ drivers/clk/clk-tc956x.c
@@ -1,5 +1,4 @@
 // SPDX-License-Identifier: GPL-2.0
-//
 
 /*
  * Copyright (C) 2026 by RISCstar Solutions Corporation.  All rights reserved.
@@ -10,24 +9,17 @@
 #include <linux/mfd/syscon.h>
 #include <linux/mod_devicetable.h>
 #include <linux/module.h>
+#include <linux/of_address.h>
 #include <linux/platform_device.h>
 #include <linux/regmap.h>
 
 #include <dt-bindings/clock/toshiba,tc9564.h>
 
-#define DRIVER_NAME		"tc956x-clk"
-
-/*
- * Clock register offsets used to enable/disable clocks.  These offsets
- * are relative to the base of the chip configuration space mapped by the
- * system controller referred to by the "toshiba,config-syscon" property.
- */
-#define CLKCTRL0_OFFSET			0x1004
-#define CLKCTRL1_OFFSET			0x100c
+#define DRIVER_NAME	"tc956x-clk"
 
 struct tc956x_clock_init {
-	const char *name;		/* NULL means unused entry */
-	u32 offset;
+	const char *name;	/* NULL means unused entry */
+	u32 zero_one;		/* Index into clocks->offset[] */
 	u32 mask;
 };
 
@@ -35,20 +27,21 @@ struct tc956x_clock {
 	struct clk_hw hw;
 	u16 which;
 	u16 offset;
-	u32 mask;			/* Zero means undefined clock */
+	u32 mask;		/* Zero means undefined clock */
 };
 
 struct tc956x_clocks {
 	struct regmap *regmap;
+	u32 offset[2];
 	size_t clock_count;
 	struct tc956x_clock clocks[] __counted_by(clock_count);
 };
 
-#define TC956X_CLOCK_INIT(_name, _zero_one, _bit)		\
-	[CLOCK_ ## _name] = {					\
-		.name	= #_name,				\
-		.offset	= CLKCTRL ## _zero_one ## _OFFSET,	\
-		.mask	= BIT(_bit),				\
+#define TC956X_CLOCK_INIT(_name, _zero_one, _bit)	\
+	[CLOCK_ ## _name] = {				\
+		.name		= #_name,		\
+		.zero_one	= _zero_one,		\
+		.mask		= BIT(_bit),		\
 	}
 
 static const struct tc956x_clock_init tc9564_clock_init[] = {
@@ -77,6 +70,10 @@ static const struct tc956x_clock_init tc9564_clock_init[] = {
 	TC956X_CLOCK_INIT(MAC1_ALL, 1, 31),
 };
 
+/* Mask that includes all meaningful bits in each clock control register */
+#define TC956X_CLOCK0_ALL_MASK	0xe7056091	/* 0xe7057291 */
+#define TC956X_CLOCK1_ALL_MASK	0xe000c080	/* 0xe000c080 */
+
 static struct tc956x_clock *hw_to_tc956x_clock(struct clk_hw *hw)
 {
 	return container_of(hw, struct tc956x_clock, hw);
@@ -90,10 +87,11 @@ static struct tc956x_clocks *tc956x_clock_to_clocks(struct tc956x_clock *clock)
 static void tc956x_clk_manage(struct tc956x_clock *clock, bool enable)
 {
 	struct tc956x_clocks *clocks = tc956x_clock_to_clocks(clock);
+	u32 offset = clock->offset;
+	u32 mask = clock->mask;
 
 	/* No errors returned for MMIO regmap */
-	regmap_update_bits(clocks->regmap, clock->offset, clock->mask,
-			   enable ? clock->mask : 0);
+	regmap_update_bits(clocks->regmap, offset, mask, enable ? mask : 0);
 }
 
 /* Prepare also includes enable */
@@ -125,21 +123,55 @@ static struct clk_ops tc956x_clk_ops = {
 	.unprepare	= tc956x_clk_unprepare,
 };
 
+static void tc956x_clock_disable_all(struct tc956x_clocks *clocks)
+{
+	struct regmap *regmap = clocks->regmap;
+
+	regmap_write(regmap, clocks->offset[0], ~TC956X_CLOCK0_ALL_MASK);
+	regmap_write(regmap, clocks->offset[1], ~TC956X_CLOCK1_ALL_MASK);
+}
+
 static int tc956x_clk_probe(struct platform_device *pdev)
 {
 	const struct tc956x_clock_init *clock_init;
 	struct device *dev = &pdev->dev;
 	struct tc956x_clocks *clocks;
 	struct tc956x_clock *clock;
+	struct device_node *np;
 	struct regmap *regmap;
 	struct clk_hw *hw;
+	u32 offset0;
+	u32 offset1;
+	u64 addr;
+	u64 size;
 	int ret;
 	u32 i;
 
 	dev_info(dev, " === %s starting\n", __func__);
 
-	regmap = syscon_regmap_lookup_by_phandle(dev_of_node(dev),
-						 "toshiba,config-syscon");
+	np = dev_of_node(dev);
+	if (!np)
+		return dev_err_probe(dev, -EINVAL, "no devicetree node\n");
+
+	ret = of_property_read_reg(np, 0, &addr, &size);
+	if (ret)
+		return dev_err_probe(dev, ret, "failed to get offset 0\n");
+
+	if (size != sizeof(offset0))
+		return dev_err_probe(dev, -EINVAL,
+				     "bad offset 0 size %llu\n", size);
+	offset0 = lower_32_bits(addr);
+
+	ret = of_property_read_reg(np, 1, &addr, &size);
+	if (ret)
+		return dev_err_probe(dev, ret, "failed to get offset 1\n");
+
+	if (size != sizeof(offset1))
+		return dev_err_probe(dev, -EINVAL,
+				     "bad offset 1 size %llu\n", size);
+	offset1 = lower_32_bits(addr);
+
+	regmap = syscon_regmap_lookup_by_phandle(np, "toshiba,config-syscon");
 	if (IS_ERR(regmap))
 		return dev_err_probe(dev, PTR_ERR(regmap),
 				     "failed to get config regmap\n");
@@ -148,12 +180,18 @@ static int tc956x_clk_probe(struct platform_device *pdev)
 	if (!clocks)
 		return dev_err_probe(dev, -ENOMEM,
 				     "failed to allocate clocks\n");
+
 	clocks->regmap = regmap;
+	clocks->offset[0] = offset0;
+	clocks->offset[1] = offset1;
 
 	clock = &clocks->clocks[0];
 	clock_init = &tc9564_clock_init[0];
-	for (i = 0; i < clocks->clock_count && clock_init->name; i++) {
+	for (i = 0; i < clocks->clock_count; i++, clock++, clock_init++) {
 		struct clk_init_data init = { };
+
+		if (!clock_init->name)
+			continue;
 
 		init.name = clock_init->name;
 		init.ops = &tc956x_clk_ops;
@@ -161,32 +199,33 @@ static int tc956x_clk_probe(struct platform_device *pdev)
 		hw = &clock->hw;
 		hw->init = &init;
 
-		ret = clk_hw_register(dev, hw);
+		ret = devm_clk_hw_register(dev, hw);
 		if (ret)
-			goto unwind;
+			return dev_err_probe(dev, ret,
+					     "failed to register clock \"%s\"\n",
+					     clock_init->name);
 
 		clock->which = i;
-		clock->offset = clock_init->offset;
+		clock->offset = clocks->offset[clock_init->zero_one];
 		clock->mask = clock_init->mask;
-
-		clock++;
-		clock_init++;
 	}
+	platform_set_drvdata(pdev, clocks);
+
+	/* Force all clocks to be initially disabled */
+	tc956x_clock_disable_all(clocks);
 
 	dev_info(dev, " === %s successful\n", __func__);
 
 	return 0;
-unwind:
-	while (--i)
-		clk_hw_unregister(--hw);
-
-	return dev_err_probe(dev, -ENOMEM, "failed to register \"%s\" clock\n",
-			     clock_init->name);
 }
 
 static void tc956x_clk_remove(struct platform_device *pdev)
 {
-	/* Nothing to do for now */
+	struct tc956x_clocks *clocks = platform_get_drvdata(pdev);
+
+	/* Leave all clocks disabled when done */
+	tc956x_clock_disable_all(clocks);
+	kfree(clocks);
 }
 
 static const struct of_device_id tc956x_clk_ids[] = {
@@ -202,7 +241,7 @@ static struct platform_driver tc956x_clk_driver = {
 		.name		= DRIVER_NAME,
 		.of_match_table = tc956x_clk_ids,
 		.owner		= THIS_MODULE,
-		// .probe_type	= PROBE_PREFER_ASYNCHRONOUS,
+		.probe_type	= PROBE_PREFER_ASYNCHRONOUS,
 	},
 };
 module_platform_driver(tc956x_clk_driver);
